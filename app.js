08c748db93af18e9ad8ec021b4b6fe7be0c4748e
Lachlan <lachlan_young@hotmail.com>
2017-11-17 16:00:02 +1000

    Read Arguments from script and build it

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 #! /usr/bin/env node
 
-console.log("Hello World");
+function buildParameters(){
+  let argEntries = process.argv;
+  let finalParameters = [];
+  for(let i = argEntries.length-1; i >= 0; i--){
+    //loop through each char, if equal to / then:
+    if(argEntries[i].indexOf("/") < 0){
+      finalParameters.push(argEntries[i]);
+    }
+  }
+
+  let searchParameters = (finalParameters.reverse()).toString();
+  searchParameters = searchParameters.replace(/,/g, "+");
+
+  return searchParameters;
+}
+
+function run(){
+  let params = buildParameters()
+  let url = "https://www.google.com.au/search?q="+ params;
+
+  console.log(url);
+}
+
+run();
